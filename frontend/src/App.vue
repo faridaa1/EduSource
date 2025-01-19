c819fd9655608f52c83e949ee98f40a2a2194367
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-01-19 23:39:30 +0000

    beginning to style header making fetch for user

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <header>
-    <img class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-
+    <img id='logo' src="/logo-light.svg" alt="EduSource" width="125" height="125" v-pre/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Profile</RouterLink>
+        <RouterLink to="/">Help</RouterLink>
+        <RouterLink to="/">Settings</RouterLink>
+        <RouterLink to="/">Sign out</RouterLink>
       </nav>
-    </div>
   </header>
-
   <RouterView />
 </template>
 
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import { RouterLink, RouterView } from 'vue-router'
+    import type { User } from './types';
+    export default defineComponent({
+      components: { RouterView },
+      async mounted(): Promise<void> {
+          let userResponse: Response = await fetch('/api/user', {
+            method: 'GET',
+            credentials: 'include'
+          })
+          let userData: { user: User } = await userResponse.json()
+          console.log(userData)
+      }
+    })
 </script>
 
-<style scoped>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+    @import url('https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css');
+    #logo {
+        height: 4rem;
+        padding:0;
+        margin: 0;
+    }
+    
+    header {
+      display: flex;
+      background-color: #0DCAF0;
+      border-radius: 1rem;
+    }
 </style>
---
+++ frontend/src/components/AuthBuyerHome.vue
@@ -0,0 +1,13 @@
+<template>
+    ok
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+    })
+</script>
+
+<style>
+    @import url('https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css');
+</style>
