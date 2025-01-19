0d3e2f7938663c6d01a92d8c77fed447738ebc21
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-01-19 18:10:11 +0000

    getting user authentication to work

--- api/forms.py
+++ api/forms.py
@@ -59,6 +59,52 @@ class SignupForm(ModelForm):
             raise ValidationError('Username cannot contain special characters')
         return username
     
+    def clean_description(self) -> ValidationError | str:
+        """Handling description validation"""
+        mode: str = self.cleaned_data['mode']
+        if mode == 'buyer':
+            return ''
+        description: str = self.cleaned_data['description']
+        return description
+    
+    def clean_password(self) -> ValidationError | str:
+        """Handling password validation"""
+        password: str = self.cleaned_data['password']
+        if re.search(r"\s", password):
+            raise ValidationError('Password cannot contain spaces')
+        if len(password) < 8 or len(password) > 15:
+            raise ValidationError('Password must be between 8 to 15 characters long')
+        return password
+    
+    def clean_reenter_password(self) -> ValidationError | str:
+        """Handling re-entered password validation"""
+        reentered_password: str = self.cleaned_data['reenter_password']
+        clean_password: str | None = self.cleaned_data.get('password', None)
+        if clean_password and not self.cleaned_data['password'] == reentered_password:
+                raise ValidationError('Both passwords must match')
+        return reentered_password
+    
+
+class LoginForm(ModelForm):
+    """Form used for logging in a user"""
+    reenter_password = forms.CharField(max_length=15, required=True)
+    class Meta:
+        model: User = User
+        fields: list[str, str] = [
+            'email',
+            'password',
+            'reenter_password'
+        ]
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                    'class' : 'form-control',
+                    'placeholder' : field
+                }
+            )
+
     def clean_password(self) -> ValidationError | str:
         """Handling password validation"""
         password: str = self.cleaned_data['password']
--- api/views.py
+++ api/views.py
@@ -1,14 +1,46 @@
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
-from .forms import SignupForm, AddressForm
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.contrib.auth.hashers import check_password
+from .forms import SignupForm, AddressForm, LoginForm
+from .models import User, Address
 
 def signup(request: HttpRequest) -> HttpResponse:
+    """Handling user sign up"""
     if request.method == 'POST':
         signup_form: SignupForm = SignupForm(request.POST)
         address_form: AddressForm = AddressForm(request.POST)
-        if SignupForm(request.POST).is_valid() and AddressForm(request.POST).is_valid():
-            print('valid') 
+        if signup_form.is_valid() and address_form.is_valid():
+            signup_data: dict[str: str] = signup_form.cleaned_data
+            address_data: dict[str: str] = address_form.cleaned_data
+            user : User = User.objects.create_user(
+                email=signup_data['email'],
+                first_name=signup_data['first_name'],
+                last_name=signup_data['last_name'],
+                phone_number=signup_data['phone_number'],
+                username=signup_data['username'],
+                password=signup_data['password'],
+                theme_preference=signup_data['theme_preference'],
+                mode=signup_data['mode'],
+                description=signup_data['description']
+            )
+            Address.objects.create(
+                first_line=address_data['first_line'],
+                second_line=address_data['second_line'],
+                city=address_data['city'],
+                postcode=address_data['postcode'],
+                user=user
+            )
+            # log in user
+            authenticated_user: User | None = authenticate(request, username=signup_data['username'], password=signup_data['password'])
+            if authenticated_user:
+                auth.login(request, authenticated_user)
             return redirect('http://localhost:5173/')
-        else:
-            return render(request, 'api/signup.html', {'signup_form' : signup_form, 'address_form' : address_form})
+        return render(request, 'api/signup.html', {'signup_form' : signup_form, 'address_form' : address_form})
     return render(request, 'api/signup.html', {'signup_form' : SignupForm(), 'address_form' : AddressForm()})
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    """Handling user log in"""
+    return render(request, 'api/login.html', {'login_form' : LoginForm()})
