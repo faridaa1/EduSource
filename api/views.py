520ffe1b382c62c1cc9aab558bb2bf3a28203c05
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-01-19 14:23:55 +0000

    refactoring

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import SignupForm, AddressForm
 
 def signup(request: HttpRequest) -> HttpResponse:
@@ -7,7 +7,8 @@ def signup(request: HttpRequest) -> HttpResponse:
         signup_form: SignupForm = SignupForm(request.POST)
         address_form: AddressForm = AddressForm(request.POST)
         if SignupForm(request.POST).is_valid() and AddressForm(request.POST).is_valid():
-            pass
+            print('valid') 
+            return redirect('http://localhost:5173/')
         else:
             return render(request, 'api/signup.html', {'signup_form' : signup_form, 'address_form' : address_form})
     return render(request, 'api/signup.html', {'signup_form' : SignupForm(), 'address_form' : AddressForm()})
