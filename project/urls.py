04d83cdfe8ce754812c41862721bce0f63ef38b3
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-03-12 20:38:56 +0000

    adding route openshift checks

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health', lambda request: HttpResponse("OK")), # https://github.qmul.ac.uk/ec22476/ECS639U-Group-Coursework/blob/main/project/urls.py
     path('', include("api.urls"))
 ]
