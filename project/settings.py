b36d1f978df173853e0944aa578a6d2a07cf2899
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-03-14 18:28:56 +0000

    attempting to get assets seen in deployment

--- project/settings.py
+++ project/settings.py
@@ -145,6 +145,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    BASE_DIR / 'frontend' / 'dist',
 ]
 
 # Default primary key field type
