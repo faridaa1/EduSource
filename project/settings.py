c677d2a2dca394bc0adf8f0d840c9593c9e2e82e
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-03-15 12:54:26 +0000

    moving files for deployment

--- project/settings.py
+++ project/settings.py
@@ -145,6 +145,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    BASE_DIR / 'static' / 'assets',
 ]
 
 # Default primary key field type
