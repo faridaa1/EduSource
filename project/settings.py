3abc8f8d0d802bec1e30c51e5b9df4af2b1edcb4
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-03-15 22:43:14 +0000

    moving files for deployment

--- project/settings.py
+++ project/settings.py
@@ -142,11 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'api' / 'static' / 'api',
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
