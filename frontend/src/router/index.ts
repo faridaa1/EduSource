f76a9ade5c62860177b4ea6825717b73fddfee51
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-03-16 12:21:13 +0000

    updating and rebuilding

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -23,9 +23,8 @@ import ChatBot from '@/components/help/ChatBot.vue'
 
 
 // Defining frontend routing
-console.log(import.meta.env.BASE_URL, import.meta.env.BASE_URL === 'development')
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL === '/' ? import.meta.env.BASE_URL : ''),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/chatbot', name: 'chatbot', component: ChatBot },
