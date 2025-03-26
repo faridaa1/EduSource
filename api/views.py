6ff94792b5280fcbcee9881d90ea8e99dc7adcf7
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-03-26 10:12:42 +0000

    updating logic

--- api/views.py
+++ api/views.py
@@ -860,10 +860,10 @@ def semantic_search(request: HttpRequest, user: int) -> JsonResponse:
                 search_dict_author[i][1]*0.1+
                 search_dict_colour[i][1]*0.1)
         sorted_search_dict = sorted(search_dict.items(), key=order_data, reverse=True)
-        threshold = 0.5
+        threshold = 0.4
         keys = []
         keys: list = [pair[0] for pair in sorted_search_dict if pair[1] >= threshold]
-        while len(keys) == 0 and threshold > 0.4:
+        while len(keys) == 0 and threshold > 0.35:
             keys: list = [pair[0] for pair in sorted_search_dict if pair[1] >= threshold]
             threshold = round(threshold - 0.01, 2)
     resources: list = []
