22b2242411533107dacaec8b65f58a195e9182e6
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-04-18 12:11:33 +0100

    beginning to add test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from api.models import User
+
+TEST_USERNAME = 'farida'
+TEST_PASSWORD = 'farida123'
+
+class UserTestCase(TestCase):
+    """Testing user signup and login"""
+    def setUp(self):
+        self.client = Client()
+        user = User.objects.create(username=TEST_USERNAME)
+        user.set_password(TEST_PASSWORD)
+        user.save()
+    
+    def test_signup(self):
+        url = reverse('api:signup')
+        print(url)
