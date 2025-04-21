0593a396073cd0b2fd858baee16a2f88a61144e3
Farida Addo <f.addo@se22.qmul.ac.uk>
2025-04-21 13:06:51 +0100

    beginning selenium tests, going back to unit testing

--- api/tests.py
+++ api/tests.py
@@ -1,13 +1,16 @@
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 from api.models import User
 
 TEST_USERNAME = 'farida'
 TEST_PASSWORD = 'farida123'
 
-class UserTestCase(TestCase):
-    """Testing user signup and login"""
+class UnitTesting(TestCase):
+    """Comprehensive unit testing"""
     def setUp(self):
         self.client = Client()
         user = User.objects.create(username=TEST_USERNAME)
@@ -17,3 +20,35 @@ class UserTestCase(TestCase):
     def test_signup(self):
         url = reverse('api:signup')
         print(url)
+
+# class FunctionalTesting(StaticLiveServerTestCase):
+#     """Functional testing in Selenium"""
+#     port = 8000
+
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.selenium = WebDriver()
+#         super().setUpClass()
+    
+#     # @classmethod
+#     # def tearDownClass(cls):
+#     #     cls.selenium.quit()
+#     #     super().tearDownClass()
+    
+#     def get_url(self, path):
+#         """Function to retrieve url given a path"""
+#         self.selenium.get(f"{self.live_server_url}/{path}/")
+#     def test_edusource(self):
+#         """Combine individual methods"""
+#         self.test_signup()
+    
+#     def test_signup(self):
+#         """Testing signup"""
+
+#         # Retrieve url
+#         self.get_url('signup')
+
+#         # Fill in fields
+#         email_field = self.selenium.find_element(By.ID, 'email')
+#         email_field.click()
+#         email_field.send_keys(TEST_EMAIL)
